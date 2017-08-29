02c419c05deaea70dc566bf56c702c13724f5c20
github username <github email address>
2017-08-29 11:46:06 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+export default class EyesOnMe extends React.Component {
+
+  render() {
+    return (
+      <button onBlur={this.blur} onFocus={this.focus}>submit</button>
+    )
+  }
+
+  focus() {
+    console.log('Good!')
+  }
+
+  blur() {
+    console.log('Hey! Eyes on me!')
+  }
+
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
 // Code Keypad Component Here
+import React from 'react'
+
+export default class Keypad extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <input type="password" onKeyUp={this.console}></input>
+      </div>
+    )
+  }
+
+  console() {
+    console.log('Entering password...')
+  }
+
+}
